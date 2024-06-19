26ac6be18b8158007768ab354e3827364daeb4dd
Siyu <42793743+Grenzlinie@users.noreply.github.com>
2024-06-19 17:31:18 +0800

    Update cf_worker.js

--- cf_worker.js
+++ cf_worker.js
@@ -1,4 +1,4 @@
-const TELEGRAPH_URL = 'https://api.openai.com';
+const TELEGRAPH_URL = 'https://open.bigmodel.cn/';
 
 addEventListener('fetch', event => {
   event.respondWith(handleRequest(event.request))
